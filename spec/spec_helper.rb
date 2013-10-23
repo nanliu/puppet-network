3ff4c5ccf9b026d7dfa037f3c920ce878df3335e
Nan Liu <nan.liu@gmail.com>
2013-10-23 15:21:50 -0700

    Fix library load.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
+Puppet[:modulepath] = './spec/fixtures/modules'
